b1057c19a9c38b1ffc984f146b947a43d7b73596
Renni-W <153299924+Renni-W@users.noreply.github.com>
2024-04-21 13:46:37 +0000

    Update ImageConstantROI_GB.py

--- ImageConstantROI_GB.py
+++ ImageConstantROI_GB.py
@@ -5,12 +5,14 @@ class ImageConstantROI():
             
             "Country_Code": [(1194, 649, 293, 150)],
             "Personal_ID_Number": [(1166, 481, 712, 80)],
-            "Line_1": [(185, 1221, 2517, 125)],
-            "Line_2": [(167, 1350, 2489, 132)],
-            "Line_3": [(174, 1490, 2478, 125)],
-            
+            "FirstNames": [(761, 1500, 670, 115)],
+            "Surname": [(174, 1497, 411, 111)],
+            "DOB": [(174, 1361, 499, 111)],
+            "Date_of_Expiry": [(844, 1354, 502, 122)],
+            "Document_number": [(593, 1218, 757, 118)]
           
         } 
         regions = [("Country_Code","Personal_ID_Number","Line_1","Line_2", 
                    "Line_3")]
         CHECK_ROI = [(313, 174, 597, 63)]
+        
